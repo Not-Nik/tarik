62521b458a689dbc9834c39cc5e224db0685f5ec
Not-Nik <nik.wipper@gmx.de>
2024-04-09 23:13:33 +0200

    Error when we encounter an unhandled expression type

--- src/semantic/Analyser.cpp
+++ src/semantic/Analyser.cpp
@@ -625,6 +625,9 @@ bool Analyser::verify_expression(Expression *expression, bool assigned_to, bool 
         case NULL_EXPR:
             expression->assign_type(Type(VOID, 1));
             break;
+        default:
+            bucket->error(expression->origin, "internal: unexpected, unhandled type of expression");
+            return false;
     }
     return true;
 }
