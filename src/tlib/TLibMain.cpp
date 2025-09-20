d05c7c0a60aaca4be4da94b2af5dbe24902f79fd
Nikolas <nik.wipper@gmx.de>
2025-09-20 20:42:38 +0200

    Add ability to merge two tlib files

--- src/tlib/TLibMain.cpp
+++ src/tlib/TLibMain.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+#include "Export.h"
 #include "Import.h"
 #include "cli/Arguments.h"
 #include "Version.h"
@@ -11,11 +12,25 @@ int main(int argc, const char *argv[]) {
     ArgumentParser parser(argc, argv, "tarik-tlib");
 
     Option *version = parser.add_option("version", "Miscellaneous", "Display the compiler version");
+    Option *merge_option = parser.add_option("merge",
+                                             "Actions",
+                                             "Merge a library into the input library",
+                                             true,
+                                             "library");
+
+    std::filesystem::path merge;
 
     for (const auto &option : parser) {
         if (option == version) {
             std::cout << version_id << " tarik compiler tester version " << version_string << "\n";
             return 0;
+        } else if (option == merge_option) {
+            merge = option.argument;
+
+            if (!exists(merge)) {
+                std::cerr << "error: Input file " << merge << " does not exist\n";
+                return 1;
+            }
         }
     }
 
@@ -31,9 +46,18 @@ int main(int argc, const char *argv[]) {
         return 1;
     }
 
-    std::vector<aast::Statement*> statements = import_statements(input);
+    std::vector<aast::Statement *> statements = import_statements(input);
 
-    for (auto *statement : statements) {
-        std::println("{}", statement->print());
+    if (!merge.empty()) {
+        std::vector<aast::Statement *> merge_statements = import_statements(input);
+
+        Export exporter;
+        exporter.generate_statements(statements);
+        exporter.generate_statements(merge_statements);
+        exporter.write_file(input);
+    } else {
+        for (auto *statement : statements) {
+            std::println("{}", statement->print());
+        }
     }
 }
